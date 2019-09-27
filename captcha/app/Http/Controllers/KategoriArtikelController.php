2e2768ef5d5a4992660c21708240622ebd92243e
ditaaaaa <45206728+ditaaaaa@users.noreply.github.com>
2019-09-27 07:23:56 -0700

    readall

---
+++ captcha/app/Http/Controllers/KategoriArtikelController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\kategori_artikel; 
+
+class KategoriArtikelController extends Controller
+{
+    public function index(){
+    	$listkategoriartikel=kategori_artikel::all();
+    	return view ('kategori_artikel.index',compact('listkategoriartikel'));
+    }
+}
